be2e1d3df8c76c9a001cfd509c64319e25404205
Brett Sun <qisheng.brett.sun@gmail.com>
2016-08-02 11:56:43 +0200

    fix(eslint-config-ascribe): Actually allow usage of built-in object prototypes after upgrade of eslint-config-airbnb-base

--- packages/eslint-config-ascribe/rules/custom.js
+++ packages/eslint-config-ascribe/rules/custom.js
@@ -16,7 +16,7 @@ module.exports = {
 
     // Allow use of Object.prototypes builtins directly
     // http://eslint.org/docs/rules/no-prototype-builtins
-    'no-prototype-builtins': [2],
+    'no-prototype-builtins': [0],
 
 
     /**
